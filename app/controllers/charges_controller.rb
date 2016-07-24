89c04c3d9c148bd5ae67192205bc21021d660a54
Peter Sonneveld <peter_tjuuh@hotmail.com>
2016-07-24 21:20:26 +0200

    Reworked views to use product information from the database

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,6 @@
 class ChargesController < ApplicationController
 	def create
+	  product = Product.find_by_sku("GROHACK1")
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
@@ -9,11 +10,14 @@ class ChargesController < ApplicationController
 	  charge = Stripe::Charge.create(
 	    :customer    => customer.id,
 	    :amount      => params[:amount],
-	    :description => 'Growth Hacking Crash Course',
+	    :description => product.full_description,
 	    :currency    => 'usd'
 	  )
 
-	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+	  	                         amount: product.price_in_cents, description: charge.description, 
+	  	                         currency: charge.currency, customer_id: customer.id, 
+	  	                         product_id: product.id, uuid: SecureRandom.uuid)
 
 	  redirect_to purchase
 
